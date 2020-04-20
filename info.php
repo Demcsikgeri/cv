845aec744720f89a725d02683670675906d53cd7
Gergely Demcsik <demcsikg@gmail.com>
2020-04-20 16:54:45 +0200

    Add initial cv website files

---
+++ info.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Két oszlop</title>
+    <link rel="stylesheet" type="text/css" href="./css/master.css">
+  </head>
+  <body>
+    <div id="kontener">
+      <div id="foKontener">
+        <div id="tartalom">
+          <h2>Itt van némi tartalom</h2>
+          <p>Ide jönne egy történet</p>
+          <h2>Itt van még egy kis tartalom</h2>
+          <p>Ez meg egy másik történet</p>
+        </div><!-- tartalom vége -->
+        <div id="menu">
+          <h3>Menü</h3>
+          <ul>
+            <li>1. menüpont</li>
+            <li>2. menüpont</li>
+            <li>3. menüpont</li>
+          </ul>
+        </div><!-- menü vége -->
+      </div><!-- foKontener vége -->
+    </div><!-- kontener vége -->
+  </body>
+</html>
